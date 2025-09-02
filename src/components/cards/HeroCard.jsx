8dfb0fdbe23f3d2853ab9948acf6f32193d5cde9
Steven Gomez <brastegocha@gmail.com>
2025-09-01 20:31:18 -0500

    Terminando algunos updates de la responsive desing

--- src/components/cards/HeroCard.jsx
+++ src/components/cards/HeroCard.jsx
@@ -22,12 +22,12 @@ import gmail from '../icons/gmail.png';
 
 const HeroCard = () => {
     return (
-        <div id="about_me" className="justify-evenly md:justify-normal w-dvw flex grow items-center md:p-3 flex-col-reverse md:flex-col text-white">
+        <div id="about_me" className="justify-end md:justify-normal w-dvw flex grow items-center md:p-3 flex-col-reverse md:flex-col text-white">
             <div className="flex flex-col justify-center md:h-full items-center md:p-3 md:flex-row">
                 <div className="pl-1 w-full h-full md:w-2/3 flex flex-col justify-center">
-                    <h1 className="text-md md:text-6xl md:p-2">Steven Gomez</h1>
-                    <h2 className="text-md md:text-4xl md:p-2">The best <i><b><u>Professional</u></b></i> you can get!</h2>
-                    <p className="text-xs md:p-2 md:text-2xl">I'm a passionate and results-driven developer from Colombia, blending creativity with code to build web applications that are clean, functional, and user-focused. By day, I work as a BTS Support and Customer Care Specialist, where I solve technical problems and support clients with precision and empathy. By night (and any spare moment), I dive deep into full stack development.</p>
+                    <h1 className="text-3xl lg:text-6xl md:p-2">Steven Gomez</h1>
+                    <h2 className="text-2xl lg:text-4xl md:p-2">The best <i><b><u>Professional</u></b></i> you can get!</h2>
+                    <p className="text-xs lg:p-2 md:text-2xl">I'm a passionate and results-driven developer from Colombia, blending creativity with code to build web applications that are clean, functional, and user-focused. By day, I work as a BTS Support and Customer Care Specialist, where I solve technical problems and support clients with precision and empathy. By night (and any spare moment), I dive deep into full stack development.</p>
                     <h3 className="text-s md:text-3xl md:p-2">Contact me!</h3>
                     <div className="flex pl-2">
                         <a className='flex w-15 h-10 pr-3 ' target="blank" href="https://github.com/stevengomez25">
@@ -50,7 +50,7 @@ const HeroCard = () => {
                     <img src={imagen} alt="heropic" className="w-full h-full  object-cover"/>
                 </div>
             </div>
-            <div className="bg-gradient-to-r from-transparent via-gray-400 to-transparent rounded-full w-full md:h-20 overflow-hidden flex items-center">
+            <div className="mt-2 md:mt-0 bg-gradient-to-r from-transparent via-gray-400 to-transparent rounded-full w-full md:h-20 overflow-hidden flex items-center">
                 <div className="flex w-full gap-10 will-change-transform
       animate-[marquee_20s_linear_infinite]
       group-hover:[animation-play-state:paused]
