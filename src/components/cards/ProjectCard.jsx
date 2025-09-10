196a65b62d4d46fb60bea4306e5cb04de51788f9
Steven Gomez <brastegocha@gmail.com>
2025-09-09 22:14:29 -0500

    finishing some bugs with gallery

--- src/components/cards/ProjectCard.jsx
+++ src/components/cards/ProjectCard.jsx
@@ -1,25 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ProjectCard = () => {
     const projects = [
-        { id: 1, title: "Proyecto 1",img:"https://i0.wp.com/netlightsystems.com/blog/wp-content/uploads/2020/06/your-website.jpg?fit=441%2C291&ssl=1", desc: "deleniti eaque eum animi exercitationem corrupti, numquam, magni facilis obcaecati quas et nesciunt nihil." },
-        { id: 2, title: "Proyecto 2",img:"https://i0.wp.com/netlightsystems.com/blog/wp-content/uploads/2020/06/your-website.jpg?fit=441%2C291&ssl=1", desc: "deleniti eaque eum animi exercitationem corrupti, numquam, magni facilis obcaecati quas et nesciunt nihil." },
-        { id: 3, title: "Proyecto 3",img:"https://i0.wp.com/netlightsystems.com/blog/wp-content/uploads/2020/06/your-website.jpg?fit=441%2C291&ssl=1", desc: "deleniti eaque eum animi exercitationem corrupti, numquam, magni facilis obcaecati quas et nesciunt nihil." },
-        { id: 4, title: "Proyecto 4",img:"https://i0.wp.com/netlightsystems.com/blog/wp-content/uploads/2020/06/your-website.jpg?fit=441%2C291&ssl=1", desc: "deleniti eaque eum animi exercitationem corrupti, numquam, magni facilis obcaecati quas et nesciunt nihil." },
-        { id: 5, title: "Proyecto 5",img:"https://i0.wp.com/netlightsystems.com/blog/wp-content/uploads/2020/06/your-website.jpg?fit=441%2C291&ssl=1", desc: "deleniti eaque eum animi exercitationem corrupti, numquam, magni facilis obcaecati quas et nesciunt nihil." },
+        { id: 1, title: "Proyecto 1", img: "https://i0.wp.com/netlightsystems.com/blog/wp-content/uploads/2020/06/your-website.jpg?fit=441%2C291&ssl=1", desc: "deleniti eaque eum animi exercitationem corrupti, numquam, magni facilis obcaecati quas et nesciunt nihil.", category: "frontend" },
+        { id: 2, title: "Proyecto 2", img: "https://i0.wp.com/netlightsystems.com/blog/wp-content/uploads/2020/06/your-website.jpg?fit=441%2C291&ssl=1", desc: "deleniti eaque eum animi exercitationem corrupti, numquam, magni facilis obcaecati quas et nesciunt nihil.", category: "frontend" },
+        { id: 3, title: "Proyecto 3", img: "https://i0.wp.com/netlightsystems.com/blog/wp-content/uploads/2020/06/your-website.jpg?fit=441%2C291&ssl=1", desc: "deleniti eaque eum animi exercitationem corrupti, numquam, magni facilis obcaecati quas et nesciunt nihil.", category: "backend" },
+        { id: 4, title: "Proyecto 4", img: "https://i0.wp.com/netlightsystems.com/blog/wp-content/uploads/2020/06/your-website.jpg?fit=441%2C291&ssl=1", desc: "deleniti eaque eum animi exercitationem corrupti, numquam, magni facilis obcaecati quas et nesciunt nihil.", category: "backend" },
+        { id: 5, title: "Proyecto 5", img: "https://i0.wp.com/netlightsystems.com/blog/wp-content/uploads/2020/06/your-website.jpg?fit=441%2C291&ssl=1", desc: "deleniti eaque eum animi exercitationem corrupti, numquam, magni facilis obcaecati quas et nesciunt nihil.", category: "softskills" },
     ];
 
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const next = () => {
-        setCurrentIndex((prev) => (prev + 1) % projects.length);
+        setCurrentIndex((prev) => (prev + 1) % filteredProjects.length);
     };
 
     const prev = () => {
         setCurrentIndex((prev) =>
-            prev === 0 ? projects.length - 1 : prev - 1
+            prev === 0 ? filteredProjects.length - 1 : prev - 1
         );
     };
+
+    const [filter, setFilter] = useState('all');
+
+    useEffect(()=>{
+        setCurrentIndex(0);
+    },[filter])
+
+    const filteredProjects = filter === 'all' ? projects : projects.filter(project => project.category === filter);
 
     // posición de cada tarjeta
     const getPosition = (index) => {
@@ -27,11 +35,11 @@ const ProjectCard = () => {
             // MAIN
             return "z-20 scale-100 opacity-100 [transform:translateX(0)_rotateY(0deg)]";
         }
-        if (index === (currentIndex - 1 + projects.length) % projects.length) {
+        if (index === (currentIndex - 1 + filteredProjects.length) % filteredProjects.length) {
             // LEFT
             return "z-10 opacity-80 scale-90 -translate-x-32 [transform:perspective(600px)_rotateY(15deg)]";
         }
-        if (index === (currentIndex + 1) % projects.length) {
+        if (index === (currentIndex + 1) % filteredProjects.length) {
             // RIGHT
             return "z-10 opacity-80 scale-90 translate-x-32 [transform:perspective(600px)_rotateY(-15deg)]";
         }
@@ -43,14 +51,23 @@ const ProjectCard = () => {
         <div id="projects" className="h-dvh w-dvw flex flex-col items-center justify-between text-black">
             {/* filtros */}
             <div className="h-1/5 flex gap-2 justify-center items-end">
-                <div className="border-2 p-2 bg-white cursor-pointer hover:scale-105">Back-End</div>
-                <div className="border-2 p-2 bg-white cursor-pointer hover:scale-105">Soft Skills</div>
-                <div className="border-2 p-2 bg-white cursor-pointer hover:scale-105">Front-End</div>
+                <button onClick={()=> setFilter('all')}>
+                    <div className="border-2 p-2 bg-white cursor-pointer hover:scale-105">All</div>
+                </button>
+                <button onClick={()=> setFilter('backend')}>
+                    <div className="border-2 p-2 bg-white cursor-pointer hover:scale-105">Back-End</div>
+                </button>
+                <button onClick={()=> setFilter('softskills')}>
+                    <div className="border-2 p-2 bg-white cursor-pointer hover:scale-105">Soft Skills</div>
+                </button>
+                <button onClick={()=> setFilter('frontend')}>
+                    <div className="border-2 p-2 bg-white cursor-pointer hover:scale-105">Front-End</div>
+                </button>
             </div>
 
             {/* carrusel */}
             <div className="relative h-3/5 w-full flex justify-center items-center overflow-hidden perspective-[1000px]">
-                {projects.map((project, i) => (
+                {filteredProjects.map((project, i) => (
                     <div
                         key={project.id}
                         className={`absolute flex flex-col text-center items-center justify-center border-2 border-black bg-white h-2/3 w-1/2 md:w-1/3 transition-all duration-700 ease-in-out ${getPosition(
