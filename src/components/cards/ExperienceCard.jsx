08a13a97dd8c1433f8c2f11ca229b24db8cb2087
Steven Gomez <brastegocha@gmail.com>
2025-09-10 22:11:17 -0500

    adding responsive just to save time later

--- src/components/cards/ExperienceCard.jsx
+++ src/components/cards/ExperienceCard.jsx
@@ -3,7 +3,7 @@ import Images from "../images/Images";
 const ExperienceCard = () => {
     return(
         <div id="experience" className="h-dvh w-full flex justify-evenly items-center">
-            <div className="w-1/4 h-4/5 flex flex-col justify-evenly items-center bg-amber-600">
+            <div className="w-3/4 md:w-1/4 h-4/5 flex flex-col justify-evenly items-center bg-amber-600">
                 <h1 className="text-3xl">FrontEnd</h1>
                 <img src={Images.img6} alt="" />
                 <div className="">Icons</div>
